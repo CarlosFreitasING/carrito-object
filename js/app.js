039ea88e70a2f15f78b67685393e175c8b8550fb
Carlos Freitas <carlosfreitasingsistemas@gmail.com>
2023-09-25 15:35:28 -0400

    cajita comentarios

--- js/app.js
+++ js/app.js
@@ -11,9 +11,9 @@ const fragment = document.createDocumentFragment();
 const btnBotones = document.querySelectorAll('.card .btn');
 
 const carritoObjeto = {};
+
 const agregarAlCarrito = (e) => {
     //console.log(e.target.dataset.fruta); 
-
     const producto = {
         titulo: e.target.dataset.fruta,
         id: e.target.dataset.fruta,
@@ -23,24 +23,24 @@ const agregarAlCarrito = (e) => {
     if(carritoObjeto.hasOwnProperty(producto.titulo)){
         producto.cantidad = carritoObjeto[producto.titulo].cantidad + 1;
     };
-
-    carritoObjeto[producto.titulo] = producto;
+    
+    carritoObjeto[producto.titulo] = producto; //carrito = {fresa = {titulo: fresa, id: fresa, cantidad: 1} }
 
     pintarCarrito();
-    console.log(carritoObjeto);
 };
 
 const pintarCarrito = () => {
-    carrito.textContent ="";
-    Object.values(carritoObjeto).forEach(item => {
-        const clone = template.content.firstElementChild.cloneNode(true);
-        clone.querySelector('.lead').textContent = item.titulo;
-        clone.querySelector('.badge').textContent = item.cantidad;
+    carrito.textContent =""; //recetea el ul
+    //Object.values(carrito) devuelve un array con todas las propiedades del objeto carrito. [titulo: fresa, id: fresa, cantidad:1],[...] = [0],[1]
+    Object.values(carritoObjeto).forEach(item => { 
+        const clone = template.content.firstElementChild.cloneNode(true); //creo el clon del template
+        clone.querySelector('.lead').textContent = item.titulo; //inserto el titulo en el clon del template
+        clone.querySelector('.badge').textContent = item.cantidad; //inserto la cantidad del clon del template
 
-        fragment.appendChild(clone);
+        fragment.appendChild(clone); //agrego el template clon editado al fragment para evitar reflow
     });
-    carrito.appendChild(fragment);
+    carrito.appendChild(fragment); //cuando termina ahora si agredo el fragmento al carrito del DOM 
 }
 
-btnBotones.forEach((btn) => btn.addEventListener("click",agregarAlCarrito));
+btnBotones.forEach((btn) => btn.addEventListener("click",agregarAlCarrito)); //agrego un adeventlistener a cada boton
 
